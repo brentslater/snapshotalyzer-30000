ca551ead85cc4cf642826a6d17c0b821ccfbc911
Brent Slater <brent.slater@brents-mbp.home>
2019-10-24 12:44:18 +1100

    List volumes and snapshots. Naive implementation of snapshot command

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -16,25 +16,96 @@ def filter_instances(project):
     return instances
 
 @click.group()
+def cli():
+    """Shotty manaages snapshots"""
+
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
+
+@snapshots.command('list')
+@click.option('--project', default=None,
+    help="Only snapshots for project (tag Project:<name>)")
+def list_snapshots(project):
+    "List EC2 snapshots"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(", ".join((
+                s.id,
+                v.id,
+                i.id,
+                s.state,
+                s.progress,
+                s.start_time.strftime("%c")
+                )))
+
+    return
+
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default=None,
+    help="Only volumes for project (tag Project:<name>)")
+def list_volumes(project):
+    "List EC2 volumes"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(", ".join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encyrpted" or "Not Encrypted"
+            )))
+
+    return
+
+@cli.group('instances')
 def instances():
     """Commands for instances"""
+
+@instances.command('snapshot',
+    help="Create snapshots of all volumes")
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def create_snapshot(project):
+    "Create snapshots for EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by Snapshot Alyzer 30000")
+
+    return
 
 @instances.command('list')
 @click.option('--project', default=None,
     help="Only instances for project (tag Project:<name>)")
 def list_instances(project):
     "List EC2 Instances"
+
     instances = filter_instances(project)
 
     for i in instances:
         tags = { t['Key']: t['Value'] for t in i.tags or [] }
-        print(','.join((
+        print(', '.join((
         i.id,
         i.instance_type,
         i.placement['AvailabilityZone'],
         i.state['Name'],
         i.public_dns_name,
-        tags.get('Project', 'No Project Tags')
+        tags.get('Project', 'None')
         )))
 
     return
@@ -69,4 +140,4 @@ def start_instances(project):
     return
 
 if __name__ == '__main__':
-    instances()
+    cli()
